a1fe517a66a610db199620b00a00625206d31637
mdebokdecostar <mdebokdecostar@gmail.com>
2026-08-18 16:15:05 +0200

    Create send-height-iframe.js

---
+++ send-height-iframe.js
@@ -0,0 +1,10 @@
+<script>
+  function sendHeight() {
+    const height = document.body.scrollHeight;
+    window.parent.postMessage({ frameHeight: height }, '*');
+  }
+
+  // Stuur de hoogte bij laden en bij scherm-resizen
+  window.addEventListener('load', sendHeight);
+  window.addEventListener('resize', sendHeight);
+</script>
