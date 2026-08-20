107928a8a1bd7a76df7167e9942ffbfaf84ad6cf
mdebokdecostar <mdebokdecostar@gmail.com>
2026-08-20 09:58:18 +0200

    Removed script tags

--- send-height-iframe.js
+++ send-height-iframe.js
@@ -1,10 +1,8 @@
-<script>
-  function sendHeight() {
-    const height = document.body.scrollHeight;
-    window.parent.postMessage({ frameHeight: height }, '*');
-  }
+function sendHeight() {
+  const height = document.body.scrollHeight;
+  window.parent.postMessage({ frameHeight: height }, '*');
+}
 
-  // Stuur de hoogte bij laden en bij scherm-resizen
-  window.addEventListener('load', sendHeight);
-  window.addEventListener('resize', sendHeight);
-</script>
+// Stuur de hoogte bij laden en bij scherm-resizen
+window.addEventListener('load', sendHeight);
+window.addEventListener('resize', sendHeight);
